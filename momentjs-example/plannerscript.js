a99c115cb801f8e8c836cf5f24f43a85685fc9ff
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-09-30 21:47:30 -0400

    Created variables in js and first changes to html

---
+++ momentjs-example/plannerscript.js
@@ -0,0 +1,41 @@
+//  Global variables 
+var currentDayEl
+var timeBlocksEl = $(".container")
+var calendarEl = $("#currentDay")
+
+// Functions
+function displayTimeBlocks
+function displayCurrentDay
+
+function retrieveEvents
+
+var events = JSON.parse(localStorage.getItem("events"));
+
+moment();
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+// Hour variables 
+
+var hour1
+
+
+var hour2 
+
+
+var hour3
+
+
+var hour4
+
+
+var hour5
+
+
+var hour6
+
+
+var hour7 
+
+
+var hour8
+
